0dab2dabce7edcee389563829181d040c1780c65
rad48 <alexandre.huet48@gmail.com>
2020-09-16 15:54:09 +0200

    test printing errors

--- app.py
+++ app.py
@@ -34,7 +34,8 @@ def predict():
 
     X['nb_patents'] = nb_patents
     X = feat_eng_new_entry(X)
-    print(X)
+    print(f'X TYPE :  {type(X)}')
+    print(f'ICCCCCCCI XXXXXX :  {X}'
     pipeline = joblib.load('bpideepmodel.joblib')
     results = pipeline.predict(X)
     return {"predictions": str(results[0])}

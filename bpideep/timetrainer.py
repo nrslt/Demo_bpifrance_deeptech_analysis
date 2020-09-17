a3f56ec2a47790637d8fb2751c23b9148a96afa8
nicolas rousselet <nicolasrousselet01@gmail.com>
2020-09-17 11:07:11 +0200

    time model

--- bpideep/timetrainer.py
+++ bpideep/timetrainer.py
@@ -1,12 +1,15 @@
 from bpideep.getdata import getjson, getfulldata
-from bpideep.feateng import feat_eng
+from bpideep.feateng import funding_amounts_employees, get_stage_age_ratio
+from bpideep.encoders import FeatEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline, make_pipeline
 from sklearn.model_selection import train_test_split, cross_val_score, cross_val_predict
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler, RobustScaler
 from sklearn.metrics import classification_report
+from sklearn.linear_model import LogisticRegression
 import numpy as np
+import pandas as pd
 import joblib
 import statsmodels.formula.api as smf
 
@@ -31,25 +34,21 @@ class Trainer():
                                 SimpleImputer(missing_values=np.nan, strategy='mean'),
                                 StandardScaler())
 
-        patent_transformer = make_pipeline(
-                                SimpleImputer(missing_values=np.nan, strategy='constant', fill_value = 0),
-                                RobustScaler())
+        # constant_adder = make_pipeline(
+        #                         smf.tools.tools.add_constant(data, prepend=True, has_constant='skip')
+        #                          )
 
-        constant_adder = make_pipeline(
-                                smf.tools.tools.add_constant(data, prepend=True, has_constant='skip')
-                                 )
-
-        features_transformer = ColumnTransformer(
-            ["constant_adder" ; constant_adder, x.columns)
-            ("feature_encoder"; FeatEncoder(), x.columns),
-            ("ratio_preproc", ratio_transformer, ['funding_employees_ratio', 'stage_age_ratio']),
-            ("patents_preproc", patent_transformer, ['nb_patents']),
-            ], remainder = 'passthrough')
+        # features_transformer = ColumnTransformer(
+        #     ["constant_adder" ; constant_adder, x.columns)
+        #     ("feature_encoder"; FeatEncoder(), x.columns),
+        #     ("ratio_preproc", ratio_transformer, ['funding_employees_ratio', 'stage_age_ratio']),
+        #     ("patents_preproc", patent_transformer, ['nb_patents']),
+        #     ], remainder = 'passthrough')
 
 
         pipemodel = Pipeline(steps=[
-                            ('features', features_transformer),
-                            ('model', smf.logit(  ))]
+                            ('ratio_transformer', ratio_transformer),
+                            ('model', LogisticRegression())]
                                          )
         self.pipeline = pipemodel
 
@@ -64,7 +63,7 @@ class Trainer():
         Save the model into a .joblib
         '''
         joblib.dump(self.pipeline, 'bpideepmodel_time.joblib')
-        print("bpideepmodel.joblib saved locally")
+        print("bpideepmodel_time.joblib saved locally")
 
 
 
@@ -72,10 +71,11 @@ if __name__ == "__main__":
 
     # importing data
     company_dict = getjson('deeptech.csv', 'non_deeptech.csv', 'almost_deeptech.csv')
-    data = getfulldata(company_dict, 'fields_list.txt')
+    X, y = getfulldata(company_dict, 'fields_list.txt')
+    X['funding_employees_ratio'] = funding_amounts_employees(X)
+    X['stage_age_ratio'] = get_stage_age_ratio(X)
+    X = X[['funding_employees_ratio', 'stage_age_ratio']]
 
-    # supposes that patents is stored as a 'patents.csv' file in /data/
-
-    t = Trainer(data)
+    t = Trainer(X, y)
     t.train()
     t.save_model()

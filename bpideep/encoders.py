cb84e8745c66d61a2ab52fecc72106ce49f5e538
nicolas rousselet <nicolasrousselet01@gmail.com>
2020-09-17 10:14:55 +0200

    few changes in feateng

--- bpideep/encoders.py
+++ bpideep/encoders.py
@@ -8,12 +8,8 @@ class FeatEncoder(BaseEstimator, TransformerMixin):
 
 
     def fit(self, X, y=None):
-        # stores the features_dict in .fit method
-        # (model fitted on whole train dataframe)
-        self.kept_cols = get_kept_cols(X)
         return self
 
     def transform(self, X, y=None):
-        # add checking on Xtest columns
         X = feat_eng(X)
         return X

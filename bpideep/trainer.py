07647c750908cae9c3fbc1cc3f2461d5fbd03ffb
nicolas rousselet <nicolasrousselet01@gmail.com>
2020-09-17 19:11:19 +0200

    custom pipe X preproc

--- bpideep/trainer.py
+++ bpideep/trainer.py
@@ -40,9 +40,13 @@ class Trainer():
             [("ratio_preproc", ratio_transformer, ['funding_employees_ratio', 'stage_age_ratio']),
              ("patents_preproc", patent_transformer, ['nb_patents'])], remainder = 'passthrough')
 
+        preprocessing = Pipeline(steps = [
+            ('featureencoder', FeatEncoder()),
+            ('features', features_transformer)
+            ])
+
         pipemodel = Pipeline(steps=[
-                            ('featureencoder', FeatEncoder()),
-                            ('features', features_transformer),
+                            ('preprocessing', preprocessing)
                             ('model', LogisticRegression(solver = 'lbfgs'))]
                             )
         self.pipeline = pipemodel
